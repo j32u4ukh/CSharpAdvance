87a87434b610c11f52993f5a8475749291b8f7b1
j32u4ukh <j32u4ukh@gmail.com>
2023-04-04 12:20:39 +0800

    feat: 寫出與讀取 json 數據

--- Program.cs
+++ Program.cs
@@ -1,15 +1,69 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Newtonsoft.Json;
+using System;
+using System.Diagnostics;
 
 namespace CSharpAdvance
 {
+    struct Data
+    {
+        public string Name;
+        public int Age;
+        public string Address;
+        public string City;
+        public string State;
+        public string Zip;
+    }
+
     class Program
     {
-        static void Main(string[] args)
+        static int Main(string[] args)
         {
+            int returnValue = 0;
+
+            // 構造要輸出的 JSON 數據
+            var data = new Data()
+            {
+                Name = "John Doe",
+                Age = 30,
+                Address = "123 Main St",
+                City = "Anytown",
+                State = "CA",
+                Zip = "12345"
+            };
+
+            // 將 JSON 數據序列化並輸出到標準輸出流
+            string json = JsonConvert.SerializeObject(data);
+            Console.WriteLine(json);
+
+            return returnValue;
+        }
+
+        static void Read()
+        {
+            Console.WriteLine("Hello world!");
+            ProcessStartInfo startInfo = new ProcessStartInfo(@"C:\Users\PC\source\repos\ConsoleApp\CSharpAdvance\bin\Debug\CSharpAdvance.exe");
+            startInfo.RedirectStandardOutput = true;
+            startInfo.UseShellExecute = false;
+
+            using (Process process = new Process())
+            {
+                process.StartInfo = startInfo;
+                process.Start();
+
+                string output = process.StandardOutput.ReadToEnd();
+                int returnValue = process.ExitCode;
+
+                // 將 JSON 數據解析為對象
+                Data data = (Data)JsonConvert.DeserializeObject(output, typeof(Data));
+
+                Console.WriteLine("Name: " + data.Name);
+                Console.WriteLine("Age: " + data.Age);
+                Console.WriteLine("Address: " + data.Address);
+                Console.WriteLine("City: " + data.City);
+                Console.WriteLine("State: " + data.State);
+                Console.WriteLine("Zip: " + data.Zip);
+                Console.WriteLine("Return value: " + returnValue);
+            }
         }
     }
 }
