039a6d94f8f0d402df5d3fe613b9fcc64562ce0a
j32u4ukh <j32u4ukh@gmail.com>
2023-04-05 16:21:21 +0800

    feat: 平行程式設計範例

--- Program.cs
+++ Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace CSharpAdvance
@@ -10,6 +11,123 @@ namespace CSharpAdvance
     {
         static void Main(string[] args)
         {
+            PLINQDemo4();
+
+            Console.WriteLine("Press any to continue...");
+            Console.ReadKey();
+        }
+
+        static void ParallelDemo()
+        {
+            int[] nums = new int[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+            int sum = 0, sumEach = 0;
+
+            // 使用 Parallel.For 方法計算陣列中所有元素的總和
+            Parallel.For(0, nums.Length, i =>
+            {
+                // 在不同的執行緒上進行計算
+                sum += nums[i];
+            });
+
+            Parallel.ForEach(nums, num =>
+            {
+                sumEach += num;
+            });
+
+            Console.WriteLine($"sum: {sum}, sumEach: {sumEach}");
+        }
+
+        static void TaskDemo()
+        {
+            // 使用 Task.Run 開始一個新的任務
+            Task task1 = Task.Run(() => {
+                Console.WriteLine("Task 1 is running on thread {0}", Thread.CurrentThread.ManagedThreadId);
+            });
+
+            // 使用 Task.Run 傳遞參數給任務
+            string message = "Hello from task 2!";
+            Task task2 = Task.Run(() => {
+                Console.WriteLine(message);
+            });
+
+            // 等待兩個任務完成
+            Task.WaitAll(task1, task2);
+
+            Console.WriteLine("All tasks are completed.");
+        }
+
+        static void PLINQDemo1()
+        {
+
+            // 生成一個從 1 到 1000000 的整數序列
+            int[] numbers = Enumerable.Range(1, 1000000).ToArray();
+
+            // 使用 AsParallel 方法將數組轉換為 PLINQ 可處理的並行類型
+            double average = numbers.AsParallel().Average();
+
+            Console.WriteLine("Average = " + average);
+        }
+
+        static void PLINQDemo2()
+        {
+            int[] numbers = Enumerable.Range(1, 100).ToArray();
+
+            var query = from n in numbers.AsParallel()
+                        where n % 2 == 0
+                        select n * n;
+
+            foreach (var result in query)
+            {
+                Console.WriteLine(result);
+            }
+
+        }
+
+        static void PLINQDemo3()
+        {
+            int[] numbers = Enumerable.Range(1, 1000000).ToArray();
+            int count = 0;
+            //int sum = numbers.AsParallel().Aggregate<int, int>(
+            //    (int)0,
+            //    (int partialSum, int i) => 
+            //    {
+            //        partialSum += i;
+            //        return partialSum;
+            //    },
+            //    (int total, int subtotal) => total + subtotal
+            //    );
+            double result = numbers.Aggregate(
+                0,
+                (total, next) =>
+                {
+                    total = total + next;
+                    count++;
+                    return total;
+                },
+                total => (double)total / count
+            );
+
+            Console.WriteLine("result = " + result);
+
+        }
+
+        static void PLINQDemo4()
+        {
+            string[] words = { "apple", "ant", "banana", "cherry", "date", "eggplant", "elephant" };
+
+            var groups = words.AsParallel().GroupBy(
+                w => w[0],
+                w => w);
+
+            foreach (var group in groups)
+            {
+                Console.WriteLine(group.Key + ":");
+                foreach (var word in group)
+                {
+                    Console.WriteLine("  " + word);
+                }
+            }
+
         }
     }
 }
