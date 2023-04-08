6ebebeccbe542162a2d894b47fa16b8e8bd9d9a1
j32u4ukh <j32u4ukh@gmail.com>
2023-04-08 11:38:41 +0800

    refactor: C#4 Product

--- Ch1.cs
+++ Ch1.cs
@@ -9,27 +9,28 @@ namespace CSharpAdvance.Ch1
 {
     public class Product
     {
-        public string Name { get; private set; }
-        public decimal Price { get; private set; }
-        public Product() { }
+        readonly string name;
+        public string Name { get { return name; } }
+        readonly decimal price;
+        public decimal Price { get { return price; } }
         public Product(string name, decimal price)
         {
-            Name = name;
-            Price = price;
+            this.name = name;
+            this.price = price;
         }
         public static List<Product> GetSampleProducts()
         {
             return new List<Product>() 
             {
-                new Product("West Side Story", 9.99m),
-                new Product("Assassins", 14.99m),
-                new Product("Frogs", 13.99m),
-                new Product("Sweeney Todd", 10.99m),
+                new Product(name: "West Side Story", price: 9.99m),
+                new Product(name: "Assassins", price: 14.99m),
+                new Product(name: "Frogs", price: 13.99m),
+                new Product(name: "Sweeney Todd", price: 10.99m),
             };
         }
         public override string ToString()
         {
-            return string.Format("{0}: {1}", Name, Price);
+            return string.Format("{0}: {1}", name, price);
         }
     }
 }
