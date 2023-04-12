ac2dcb7466eefe5516500d5a76503110b7f017df
j32u4ukh <j32u4ukh@gmail.com>
2023-04-12 18:00:40 +0800

    feat: 71. Simplify Path

--- Solution.cs
+++ Solution.cs
@@ -3,88 +3,183 @@
     using System;
     using System.Collections.Generic;
     using System.Linq;
+    using System.Text;
+
+    public class Node
+    {
+        public Node root = null;
+        public string Name { get; private set; }
+        public List<Node> nodes;
+
+        public Node(Node root, string name) 
+        {
+            this.root = root;
+            Name = name;
+            nodes = new List<Node>();
+        }
+
+        public void AddNode(string name)
+        {
+            nodes.Add(new Node(this, name));
+        }
+
+        public void AddNode(Node node)
+        {
+            node.root = this;
+            nodes.Add(node);
+        }
+
+        public override string ToString()
+        {
+            List<Node> nodes = new List<Node>() { this };
+            Node node = this.root;
+            while(node != null)
+            {
+                nodes.Insert(0, node);
+                node = node.root;
+            }
+            StringBuilder sb = new StringBuilder();
+            int i, len = nodes.Count;
+            sb.Append("/");
+            if (len > 1)
+            {
+                node = nodes[1];
+                sb.Append($"{node.Name}");
+
+                for (i = 2; i < len; i++)
+                {
+                    node = nodes[i];
+                    sb.Append($"/{node.Name}");
+                }
+            }
+            return sb.ToString();
+        }
+    }
 
     public class Solution
     {
-        //[Test("()", true)]
-        //[Test("()[]{}", true)]
-        //[Test("(]", false)]
-        [Test("([]){}", true)]
-        [Test("(){}", true)]
-        [Test("({})", true)]
-        [Test("(", false)]
-        public bool IsValid(string s)
+        [Test("/home/", "/home")]
+        [Test("/../", "/")]
+        [Test("/..", "/")]
+        [Test("/home//foo/", "/home/foo")]
+        [Test("/.../hr", "/.../hr")]
+        [Test("/home/a/../b", "/home/b")]
+        [Test("/a/./b/../../c/", "/c")]
+        [Test("/...", "/...")]
+        [Test("/.", "/")]
+        [Test("/a/../../b/../c//.//", "/c")]
+        [Test("/a//b////c/d//././/..", "/a/b/c")]
+        [Test("/..hidden", "/..hidden")]
+        [Test("/hello../world", "/hello../world")]
+        [Test("/home/../../..", "/")]
+        [Test("/home/of/foo/../../bar/../../is/./here/.", "/is/here")]
+        public string SimplifyPath(string path)
         {
-            int len = s.Length;
-            if(len == 0)
+            List<string> nodes = new List<string>();
+            // state | 0: /, 1: ., 2: default
+            int i, len = path.Length, state = 0;
+            string buffer = "";
+            char c;
+            for(i = 0; i < len; i++)
             {
-                return true;
+                c = path[i];
+                switch (state)
+                {
+                    // '/'
+                    case 0:
+                        switch (c)
+                        {
+                            case '/':
+                                break;
+                            // '/' -> '.'
+                            case '.':
+                                state = 1;
+                                buffer = ".";
+                                break;
+                            default:
+                                state = 2;
+                                buffer = c.ToString();
+                                break;
+                        }
+                        break;
+                    // '.'
+                    case 1:
+                        switch (c)
+                        {
+                            // '.' -> '/'
+                            case '/':
+                                state = 0;
+
+                                switch (buffer)
+                                {
+                                    case ".":
+                                        break;
+                                    case "..":
+                                        // 往上一層
+                                        nodes = nodes.GetRange(0, Math.Max(nodes.Count - 1, 0));
+                                        break;
+                                    default:
+                                        nodes.Add(buffer);
+                                        break;
+                                }
+                                buffer = "";
+                                break;
+                            case '.':
+                                state = 1;
+                                buffer += ".";
+                                break;
+                            default:
+                                state = 2;
+                                buffer += c.ToString();
+                                break;
+                        }
+                        break;
+                    default:
+                        switch (c)
+                        {
+                            case '/':
+                                state = 0;
+                                nodes.Add(buffer);
+                                buffer = "";
+                                break;
+                            case '.':
+                                state = 1;
+                                buffer += ".";
+                                break;
+                            default:
+                                state = 2;
+                                buffer += c.ToString();
+                                break;
+                        }
+                        break;
+                }
             }
-            // 若 s 長度為奇數，則返回 false
-            else if ((len & 1) == 1)
+            switch (state)
             {
-                return false;
-            }
-            char left = s[0], right;
-            switch(left)
-            {
-                case '(':
-                    right = ')';
+                // '/'
+                case 0:
                     break;
-                case '[':
-                    right = ']';
-                    break;
-                case '{':
-                    right = '}';
+                // '.'
+                case 1:
+                    switch (buffer)
+                    {
+                        case ".":
+                            break;
+                        case "..":
+                            // 往上一層
+                            nodes = nodes.GetRange(0, Math.Max(nodes.Count - 1, 0));
+                            break;
+                        default:
+                            nodes.Add(buffer);
+                            break;
+                    }
                     break;
                 default:
-                    return false;
+                    nodes.Add(buffer);
+                    break;
             }
-            int i, count = 1;
-            for(i = 1; i < len; i++)
-            {
-                if(s[i] == left)
-                {
-                    count++;
-                }
-                else if(s[i] == right)
-                {
-                    count--;
-
-                    if(count == 0)
-                    {
-                        break;
-                    }
-                }
-            }
-
-            if(count != 0)
-            {
-                return false;
-            }
-
-            if(i == 1)
-            {
-                if (i + 1 < len)
-                {
-                    return true && IsValid(s.Substring(i + 1, len - 2));
-                }
-                else
-                {
-                    return true;
-                }
-            }
-            else
-            {
-                if (i + 1 < len)
-                {
-                    return true && IsValid(s.Substring(1, i - 1)) && IsValid(s.Substring(i + 1, len - i - 1));
-                }
-                else
-                {
-                    return true && IsValid(s.Substring(1, len - 2));
-                }
-            }
+            string joinPath = string.Join("/", nodes);
+            return $"/{joinPath}";
         }
 
         //[Test(new int[] { 8, 6, 1, 4, 7 }, new int[] { 1, 4, 6, 7, 8 })]
