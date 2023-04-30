ed35b4ef049856803639ce483da1fda54263c709
j32u4ukh <j32u4ukh@gmail.com>
2023-04-30 22:38:04 +0800

    feat: demo RockPaperScissors

--- Program.cs
+++ Program.cs
@@ -31,8 +31,67 @@ namespace CSharpAdvance
                     }
                 }
             }
+        }
 
-            Console.Write("Press any key to exit...");
+        public static void RockPaperScissors()
+        {
+            Random random = new Random();
+            string input;
+            int rps, wins = 0, draws = 0, losses = 0;
+
+            do
+            {
+                // 1: rock, 2: paper, 3: scissors
+                Console.Write("Choose [r]ock, [p]aper, [s]cissors, or [e]xit:");
+                input = Console.ReadLine().Trim();
+                rps = random.Next(1, 4);
+
+                switch (input)
+                {
+                    case "r":
+                        rps += 10;
+                        break;
+                    case "p":
+                        rps += 20;
+                        break;
+                    case "s":
+                        rps += 30;
+                        break;
+                    case "e":
+                        Console.Clear();
+                        goto EndOfGame;
+                    default:
+                        break;
+                }
+
+                switch (rps)
+                {
+                    case 11:
+                    case 22:
+                    case 33:
+                        Console.WriteLine("This game was a draw.");
+                        draws++;
+                        break;
+                    case 12:
+                    case 23:
+                    case 31:
+                        Console.WriteLine("You lose.");
+                        losses++;
+                        break;
+                    case 13:
+                    case 32:
+                    case 21:
+                        Console.WriteLine("You win.");
+                        wins++;
+                        break;
+                    default:
+                        break;
+                }
+
+                Console.WriteLine($"Score: {wins} wins, {losses} losses, {draws} draws");
+                EndOfGame:;
+            }
+            while (input != "e");
         }
     }
 
@@ -41,7 +100,9 @@ namespace CSharpAdvance
 
         static void Main(string[] args)
         {
-            Demo.GuessANumber();
+            Demo.RockPaperScissors();
+
+            Console.Write("Press any key to exit...");
             Console.ReadKey();
         }
     }
